0de8f26eb4dfb2e2ded01331047ea3136dbfa31e
MeVishvekMe <methwani.vishvek09@gmail.com>
2024-05-26 15:23:34 +0530

    Optimal Solution for Question 88

--- Question88/src/Wa.java
+++ Question88/src/Wa.java
@@ -1,34 +1,37 @@
-import java.util.Arrays;
-
 public class Wa {
+
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        if (n == 0)
+
+        if (n == 0) {
             return;
-        int i = 0;
-        int j = 0;
-        while (i < nums1.length) {
-            if (nums1[i] == 0) {
-                j = 0;
-                while (j < nums2.length) {
-                    nums1[i] = nums2[j];
-                    j++;
-                    i++;
-                }
-                break;
+        }
+        int i = m + n - 1;
+
+        // ptr1 and ptr2 are the indices of real end numbers of both the arrays.
+        int ptr1 = m - 1;
+        int ptr2 = n - 1;
+
+        // we are not changing the nums1[ptr1] or nums2[ptr2] because we're never going to compare those elements again
+        // eventually the i pointer will come to ptr1, and we're not comparing i.
+        while(ptr2 >= 0 && ptr1 >= 0 && m > 0 && n > 0) {
+            if (nums1[ptr1] > nums2[ptr2]) {
+                nums1[i] = nums1[ptr1];
+                ptr1--;
             }
-            if (nums1[i] > nums2[j]) {
-                int temp = nums1[i];
-                nums1[i] = nums2[j];
-                nums2[j] = temp;
-                while(j + 1 < nums2.length && nums2[j + 1] < nums2[j]) {
-                    temp = nums2[j];
-                    nums2[j] = nums2[j + 1];
-                    j++;
-                }
-                j = 0;
+            else {
+                nums1[i] = nums2[ptr2];
+                ptr2--;
             }
-            i++;
+            i--;
+        }
+
+        // placing all the remaining elements of the nums2 in nums1
+        while(ptr2 >= 0) {
+            nums1[i] = nums2[ptr2];
+            ptr2--;
+            i--;
         }
         System.out.println(Arrays.toString(nums1));
     }
+
 }
