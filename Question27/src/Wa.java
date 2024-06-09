63480fe5524c5e0092bbbec59db7ab62ea753683
MeVishvekMe <methwani.vishvek09@gmail.com>
2024-06-09 20:21:56 +0530

    optimised solution

--- Question27/src/Wa.java
+++ Question27/src/Wa.java
@@ -1,33 +1,13 @@
 public class Wa {
     public int removeElement(int[] nums, int val) {
-        int left = 0;
-        int right = nums.length - 1;
+        int i = 0;
         int k = 0;
-        if(nums.length == 1) {
-            if (nums[0] == val) {
-                return k;
-            }
-            return ++k;
-        }
-        while(left < right) {
-            while(nums[left] != val) {
+        while(i < nums.length) {
+            if(nums[i] != val) {
+                nums[k] = nums[i];
                 k++;
-                left++;
-                if (left == nums.length)
-                    return k;
             }
-            while(nums[right] == val) {
-                right--;
-                if (right == 0)
-                    return k;
-            }
-            if(left >= right) {
-                return k;
-            }
-            k++;
-            nums[left] = nums[right];
-            nums[right] = val;
-            left++;
+            i++;
         }
         return k;
     }
