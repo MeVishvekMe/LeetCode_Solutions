6eb756cf251c9a6d0f217a5118c37884189256ee
MeVishvekMe <methwani.vishvek09@gmail.com>
2024-06-21 10:10:10 +0530

    using try catch (not optimized)

--- Question14/src/Solution.java
+++ Question14/src/Solution.java
@@ -1,5 +1,25 @@
 public class Solution {
     public String longestCommonPrefix(String[] strs) {
-        
+        String result = "";
+        try {
+            int i = 0;
+            int j = 0;
+            while(true) {
+                i = 0;
+                char ch = strs[0].charAt(j);
+                while(i < strs.length) {
+                    if(strs[i].charAt(j) != ch) {
+                        return result;
+                    }
+                    i++;
+                }
+                result = result + strs[0].charAt(j);
+                j++;
+            }
+
+        } catch (StringIndexOutOfBoundsException e) {
+
+        }
+        return result;
     }
 }
