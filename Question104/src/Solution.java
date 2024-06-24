6bb149122945b517a00dfef2e99ed8196307e0a1
MeVishvekMe <methwani.vishvek09@gmail.com>
2024-06-24 13:01:58 +0530

    more condensed version

--- Question104/src/Solution.java
+++ Question104/src/Solution.java
@@ -1,21 +1,9 @@
 public class Solution {
-    int depthVar = 1;
     public int maxDepth(TreeNode root) {
-        if(root == null)
+        if (root == null) {
             return 0;
-        depth(root, 1);
-        return depthVar;
-    }
-    public void depth(TreeNode root, int depth) {
-        if(root.left == null && root.right == null) {
-            if(depth > depthVar) {
-                depthVar = depth;
-            }
-            return;
+        } else {
+            return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
         }
-        if(root.left != null)
-            depth(root.left, depth + 1);
-        if(root.right != null)
-            depth(root.right, depth + 1);
     }
 }
