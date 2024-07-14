4b7a7fae3cf4e9ccdb029d806e71bca54f260edc
Vishvek Methwani <methwani.vishvek09@gmail.com>
2024-07-14 14:43:55 +0530

    better than last one

--- Question20/src/Solution.java
+++ Question20/src/Solution.java
@@ -1,50 +1,30 @@
 import java.util.Stack;
 
 public class Solution {
-  public boolean isValid(String s) {
-    if (s.isEmpty() || (s.length() % 2 != 0))
-      return false;
-    Stack<Character> stack = new Stack<>();
-    int i = 0;
-    while(i < s.length()) {
-      if(stack.isEmpty()) {
-        if(s.charAt(i) == ')' || s.charAt(i) == '}' || s.charAt(i) == ']')
-          return false;
-        stack.add(s.charAt(i));
-        i++;
-        continue;
-      }
-      char current = stack.peek();
-      if(s.charAt(i) != giveExit(current)) {
-        if(s.charAt(i) == ')' || s.charAt(i) == '}' || s.charAt(i) == ']')
-          return false;
-        else
-          stack.add(s.charAt(i));
-      }
-      else
-        stack.pop();
-      i++;
+    public boolean isValid(String s) {
+        if(s.isEmpty() || s.length() % 2 != 0)
+            return false;
+        Stack<Character> stack = new Stack<>();
+        for(char c : s.toCharArray()) {
+            if(stack.isEmpty()) {
+                if(c == ')' || c == '}' || c == ']')
+                    return false;
+                stack.add(c);
+            }
+            else {
+                if(c == '(' || c == '{' || c == '[')
+                    stack.add(c);
+                else {
+                    char top = stack.peek();
+                    if((top == '(' && c == ')') || (top == '{' && c == '}') || (top == '[' && c == ']')) {
+                        stack.pop();
+                    }
+                    else {
+                        return false;
+                    }
+                }
+            }
+        }
+        return stack.isEmpty();
     }
-    return stack.isEmpty();
-  }
-
-  public char giveExit(char c) {
-    if(c == '(')
-      return ')';
-    else if(c == '{')
-      return '}';
-    else
-      return ']';
-  }
-
 }
-// 40 -> (
-// 41 -> )
-
-// 91 -> [
-// 93 -> ]
-
-// 123 -> {
-// 125 -> }
-
-// ({}[])
