ad4010d3bfc15f145e13c6f0e52935655aeff75d
Vishvek Methwani <methwani.vishvek09@gmail.com>
2024-07-14 14:50:41 +0530

    best approach

--- Question20/src/Solution.java
+++ Question20/src/Solution.java
@@ -2,28 +2,20 @@ import java.util.Stack;
 
 public class Solution {
     public boolean isValid(String s) {
-        if(s.isEmpty() || s.length() % 2 != 0)
+        if(s.length() % 2 != 0)
             return false;
         Stack<Character> stack = new Stack<>();
         for(char c : s.toCharArray()) {
-            if(stack.isEmpty()) {
-                if(c == ')' || c == '}' || c == ']')
-                    return false;
-                stack.add(c);
-            }
-            else {
-                if(c == '(' || c == '{' || c == '[')
-                    stack.add(c);
-                else {
-                    char top = stack.peek();
-                    if((top == '(' && c == ')') || (top == '{' && c == '}') || (top == '[' && c == ']')) {
-                        stack.pop();
-                    }
-                    else {
-                        return false;
-                    }
-                }
-            }
+            if(c == '(' || c == '{' || c == '[')
+                stack.push(c);
+            else if(c == ')' && !stack.isEmpty() && stack.peek() == '(')
+                stack.pop();
+            else if(c == '}' && !stack.isEmpty() && stack.peek() == '{')
+                stack.pop();
+            else if(c == ']' && !stack.isEmpty() && stack.peek() == '[')
+                stack.pop();
+            else
+                return false;
         }
         return stack.isEmpty();
     }
