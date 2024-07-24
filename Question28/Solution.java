55d25bd1985db9c9e83e1d1f52bda228cbbdae4b
Vishvek Methwani <methwani.vishvek09@gmail.com>
2024-07-24 13:14:57 +0530

    optimized solution

---
+++ Question28/Solution.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public int strStr(String haystack, String needle) {
+        if(needle.isEmpty()) 
+            return 0;
+        int i = 0;
+        int j = 0;
+        while(i <= haystack.length() - needle.length()) {
+            j = 0;
+            while(j < needle.length() && (haystack.charAt(i + j) == needle.charAt(j))) {
+                j++;
+                if(j == needle.length()) {
+                    return i;
+                }
+            }
+            i++;
+        }
+        return -1;
+    }
+}
