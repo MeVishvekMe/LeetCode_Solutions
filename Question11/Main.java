3c1f6e74a31270276f9dc91079c18e99500f9d26
Vishvek Methwani <methwani.vishvek09@gmail.com>
2024-07-24 20:22:27 +0530

    Optimized solution

---
+++ Question11/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.maxArea(new int[]{1,1}));
+    }
+}
---
+++ Question11/Solution.java
@@ -0,0 +1,18 @@
+public class Solution {
+    public int maxArea(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        int result = 0;
+        while(left < right) {
+            int area = Math.min(height[left], height[right]) * (right - left);
+            result = Math.max(result, area);
+            if(height[left] < height[right]) {
+                left++;
+            }
+            else {
+                right--;
+            }
+        }
+        return result;
+    }
+}
