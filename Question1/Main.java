695a58cd9c2125a2bd13e0d65e2864189f002ab8
Vishvek Methwani <methwani.vishvek09@gmail.com>
2024-08-02 20:22:08 +0530

    optimized solution

---
+++ Question1/Main.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(
+            new Solution().twoSum(new int[]{2,7,11,5}, 9)
+        ));
+    }
+}
