0a2be3a5da5baf63d8bf2e7c20274e42d089f200
Vishvek Methwani <methwani.vishvek09@gmail.com>
2024-08-03 14:00:43 +0530

    optmized solution

---
+++ Question226/Solution.java
@@ -0,0 +1,18 @@
+public class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        
+        // Swap left and right children
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+
+        // Recursively invert the left and right subtrees
+        invertTree(root.left);
+        invertTree(root.right);
+
+        return root;
+    }
+}
