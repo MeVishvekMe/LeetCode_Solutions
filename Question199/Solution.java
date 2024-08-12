e22713b4a1521a8ead68debcd594e49e6cfa09a4
Vishvek Methwani <methwani.vishvek09@gmail.com>
2024-08-12 15:46:47 +0530

    optimized solution

--- Question199/Solution.java
+++ Question199/Solution.java
@@ -1,39 +1,20 @@
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Queue;
 
-public class Solution {
+class Solution {
     public List<Integer> rightSideView(TreeNode root) {
-        if(root == null) {
-            return new ArrayList<>();
-        }
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.add(root);
-        List<Integer> result = helper(queue, new ArrayList<>());
+        List<Integer> result = new ArrayList<>();
+        helper(root, result, 0);
         return result;
     }
-
-    public List<Integer> helper(Queue<TreeNode> queue, List<Integer> result) {
-        if(queue.isEmpty()) {
-            return result;
+    public void helper (TreeNode root, List<Integer> result, int depth) {
+        if(root == null) {
+            return;
         }
-        int size = queue.size();
-        int i = 0;
-        while(i < size) {
-            TreeNode current = queue.poll();
-            if(current.left != null) {
-                queue.add(current.left);
-            }
-            if(current.right != null) {
-                queue.add(current.right);
-            }
-            if(i == (size - 1)) {
-                result.add(current.val);
-            }
-            i++;
+        if(result.size() == depth) {
+            result.add(root.val);
         }
-        helper(queue, result);
-        return result;
+        helper(root.right, result, depth + 1);
+        helper(root.left, result, depth + 1);
     }
 }
