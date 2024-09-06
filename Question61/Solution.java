712b561fb2b08c6d684f728b30c95b0a8d9d09d1
Vishvek Methwani <methwani.vishvek09@gmail.com>
2024-09-06 14:42:23 +0530

    optimized solution

--- Question61/Solution.java
+++ Question61/Solution.java
@@ -1,10 +1,17 @@
 public class Solution {
     public ListNode rotateRight(ListNode head, int k) {
+        if(head == null || head.next == null) {
+            return head;
+        }
         int n = 0;
         ListNode temp = head;
         while(temp != null) {
             temp = temp.next;
             n++;
+        }
+        k = k % n;
+        if(k == 0) {
+            return head;
         }
         int i = 1;
         ListNode p = head;
