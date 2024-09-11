e7cf3eac1d3ac9eefdcd1b06ac3780f4922d1e5c
Vishvek Methwani <methwani.vishvek09@gmail.com>
2024-09-11 11:23:10 +0530

    optimized solution

---
+++ Question238/Main.java
@@ -0,0 +1,7 @@
+import java.util.Arrays;
+
+class Main {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(new Solution().productExceptSelf(new int[]{1,2,3,4})));
+    }
+}
