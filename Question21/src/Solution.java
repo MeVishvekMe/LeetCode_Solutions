30abd9d66265162ca46f9a47b042841fbcb8f863
MeVishvekMe <methwani.vishvek09@gmail.com>
2025-02-10 20:58:00 +0530

    o(1) space solution

--- Question21/src/Solution.java
+++ Question21/src/Solution.java
@@ -1,24 +1,27 @@
-public class Solution {
+class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        ListNode result = new ListNode(-1);
-        ListNode temp = result;
-        while(list1 != null && list2 != null) {
-            if(list1.val <= list2.val) {
-                temp.next = list1;
-                list1 = list1.next;
+        // Create a dummy node to simplify the merging process
+        ListNode dummy = new ListNode(-1);
+        // Pointer 'temp' will be used to build the merged list
+        ListNode temp = dummy;
+
+        // Traverse both lists while neither is exhausted
+        while (list1 != null && list2 != null) {
+            // Compare current nodes of both lists and attach the smaller one
+            if (list1.val <= list2.val) {
+                temp.next = list1;  // Attach list1 node to merged list
+                list1 = list1.next; // Move list1 pointer forward
+            } else {
+                temp.next = list2;  // Attach list2 node to merged list
+                list2 = list2.next; // Move list2 pointer forward
             }
-            else {
-                temp.next = list2;
-                list2 = list2.next;
-            }
-            temp = temp.next;
+            temp = temp.next; // Move temp pointer forward in the merged list
         }
-        if(list1 != null) {
-            temp.next = list1;
-        }
-        else if(list2 != null) {
-            temp.next = list2;
-        }
-        return result.next;
+
+        // If one of the lists is not exhausted, attach the remaining part to merged list
+        temp.next = (list1 != null) ? list1 : list2;
+
+        // Return the merged list starting from the first real node (skipping dummy node)
+        return dummy.next;
     }
 }
