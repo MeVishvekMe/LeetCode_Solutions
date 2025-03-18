311805e6b0942483dc44dd8bdee30de8355b11eb
MeVishvekMe <methwani.vishvek09@gmail.com>
2025-03-18 07:55:11 +0530

    added comments

--- Question102/src/Solution.java
+++ Question102/src/Solution.java
@@ -3,29 +3,31 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
-public class Solution {
+class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
-        if(root == null) {
-            return new ArrayList<>();
-        }
-        List<List<Integer>> resultList = new ArrayList<>();
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.add(root);
-        while(!queue.isEmpty()) {
-            int i = 0;
-            int size = queue.size();
-            List<Integer> list = new ArrayList<>();
-            while(i < size) {
-                TreeNode temp = queue.poll();
-                list.add(temp.val);
-                if(temp.left != null)
-                    queue.add(temp.left);
-                if(temp.right != null)
-                    queue.add(temp.right);
-                i++;
+        List<List<Integer>> result = new ArrayList<>(); // Stores the final level order traversal
+
+        // If the tree is empty, return an empty list
+        if (root == null) return result;
+
+        Queue<TreeNode> queue = new LinkedList<>(); // Queue to store nodes for BFS
+        queue.offer(root); // Start with the root node
+
+        while (!queue.isEmpty()) { // Process nodes level by level
+            int levelSize = queue.size(); // Number of nodes in the current level
+            List<Integer> levelNodes = new ArrayList<>(); // List to store values of nodes in the current level
+
+            // Process all nodes in the current level
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode current = queue.poll(); // Get the front node from the queue
+                levelNodes.add(current.val); // Add the node's value to the level list
+
+                // Add left and right children to the queue if they exist
+                if (current.left != null) queue.offer(current.left);
+                if (current.right != null) queue.offer(current.right);
             }
-            resultList.add(list);
+            result.add(levelNodes); // Add the current level's nodes to the result list
         }
-        return resultList;
+        return result; // Return the level order traversal result
     }
 }
